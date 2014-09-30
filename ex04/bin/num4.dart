2dd2714295ee0e46c67e07990fc17ef364819208
Dzenan Ridjanovic <dzenanr@gmail.com>
2014-09-30 20:25:35 +0200

    add dr.txt

--- ex04/bin/num4.dart
+++ ex04/bin/num4.dart
@@ -1,19 +1,19 @@
 // Numéro 4
 import 'dart:math';
-main () {
 
-var II = 1000;
-var y = 5;
-var r = 1.048;
-var first = (II * r);
-var second = II * pow(r,2);
-var third = II * pow(r,3);
-var fourth = II * pow(r,4);
-var fifth = II * pow(r,5);
-
-print ('Montant après la premiere annee : $first dollars');
-print ('Montant après la deuxième annee : $second dollars');
-print ('Montant après la troisième annee : $third dollars');
-print ('Montant après la quatrième annee : $fourth dollars');
-print ('Montant après la cinquième annee : $fifth dollars');
+main() {
+  var II = 1000;
+  var y = 5;
+  var r = 1.048;
+  var first = (II * r);
+  var second = II * pow(r, 2);
+  var third = II * pow(r, 3);
+  var fourth = II * pow(r, 4);
+  var fifth = II * pow(r, 5);
+  
+  print ('Montant après la premiere annee : $first dollars');
+  print ('Montant après la deuxième annee : $second dollars');
+  print ('Montant après la troisième annee : $third dollars');
+  print ('Montant après la quatrième annee : $fourth dollars');
+  print ('Montant après la cinquième annee : $fifth dollars');
 }
